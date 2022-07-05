8ec9c5ba718c92a614e025ee1092e91a14f61b61
Sebastien <seb_bielecki@hotmail.com>
2022-07-04 20:13:34 -0500

    mvp completed

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require('@nomiclabs/hardhat-waffle');
 require("dotenv").config({ path: ".env" });
+require("@nomiclabs/hardhat-etherscan");
 
 module.exports = {
   solidity: '0.8.1',
@@ -8,5 +9,10 @@ module.exports = {
       url: process.env.ALCHEMY_API_KEY_URL,
       accounts: [process.env.RINKEBY_PRIVATE_KEY],
     },
-  },
+    },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  }
 };
